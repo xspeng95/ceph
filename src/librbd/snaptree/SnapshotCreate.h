cbf71fc3956a367470aff72b7410594f69fb8d25
xspeng95 <873806889@qq.com>
2021-03-05 15:39:54 +0800

    add snaptree

---
+++ src/librbd/snaptree/SnapshotCreate.h
@@ -0,0 +1,30 @@
+////
+//// Created by xspeng on 2021/2/26.
+////
+//
+//#ifndef CEPH_LIBRBD_SNAP_TREE_SNAPSHOT_CREATE_REQUEST_H
+//#define CEPH_LIBRBD_SNAP_TREE_SNAPSHOT_CREATE_REQUEST_H
+//
+//namespace librbd{
+//
+//    class Context;
+//    class RWLock;
+//    class ImageCtx;
+//
+//    namespace SnapTree{
+//        class SnapshotCreate {
+//        public:
+//            SnapshotCreateRequest(ImageCtx &image_ctx, ceph::shared_mutex* snap_tree_lock,
+//                    uint64_t snap_id,
+//                    Context * on_finish)
+//                    :{
+//
+//            }
+//        };
+//    }
+//
+//}
+//
+//
+//
+//#endif //CEPH_LIBRBD_SNAP_TREE_SNAPSHOT_CREATE_REQUEST_H
